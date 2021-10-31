48a0699b96ad0b45df15032e44c79dd767e9fe6f
cs20200665 <dimenerno@kaist.ac.kr>
2021-10-31 16:24:59 +0900

    Implemented dog-breed and dog-sns

--- src/components/DogSNS.js
+++ src/components/DogSNS.js
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from 'react';
+import LikeButton from './LikeButton';
 
 const DogSNS = () => {
   const breeds = ['shiba', 'terrier', 'retriever', 'husky', 'chihuahua', 'beagle'];
+  const numOfImages = 5
   const [dogs, setDogs] = useState([]);
   const [breed, setBreed] = useState(breeds[0]);
+  const [likes, setLikes] = useState(new Array(numOfImages))
+  const [numLikes, setNumLikes] = useState(0);
 
   useEffect(() => {
-    fetch(`https://dog.ceo/api/breed/${breed}/images/random/5`)
+    fetch(`https://dog.ceo/api/breed/${breed}/images/random/${numOfImages}`)
       .then(res => res.json())
       .then(result => {
         setDogs(result.message)
+        setLikes(_ => _.fill(false))
+        setNumLikes(0)
       })
   }, [breed])
 
@@ -18,11 +24,26 @@ const DogSNS = () => {
       {breeds.map(breed =>
         <button key={breed} onClick={() => setBreed(breed)}>{breed}</button>
       )}
+      <div className='number-of-likes'>
+        {numLikes}
+      </div>
       <div className='dogsns-list'>
         {dogs.length !== 0 &&
-          dogs.map(url =>
+          dogs.map((url, idx) =>
             <div className='dogsns-row' key={url}>
               <img src={url} alt="Dog" width="300" />
+              <LikeButton isLiked={likes[idx]} onClick={() => 
+                { 
+                  if (likes[idx] === true) {
+                    setLikes(_ => {_[idx] = false; return _})
+                    setNumLikes(numLikes - 1)
+                  }
+                  else {
+                    setLikes(_ => {_[idx] = true; return _})
+                    setNumLikes(numLikes + 1)
+                  }
+                } 
+              }/>
             </div>
           )
         }
--- src/components/DogsByBreed.js
+++ src/components/DogsByBreed.js
@@ -3,20 +3,22 @@ import './DogsByBreed.css';
 
 const DogsByBreed = () => {
   const breeds = ['shiba', 'terrier', 'retriever', 'husky', 'chihuahua', 'beagle'];
-  const [dogs, setDogs] = useState([]);
+  const [dogs, setDogs] = useState([]); // Image storage
+  const [breed, setBreed] = useState('shiba')
+
 
   useEffect(() => {
-    fetch(`https://dog.ceo/api/breed/shiba/images/random/5`)
+    fetch(`https://dog.ceo/api/breed/${breed}/images/random/5`)
       .then(res => res.json())
       .then(result => {
         setDogs(result.message)
       })
-  }, [])
+  }, [breed])
 
   return (
     <div>
       {breeds.map(breed =>
-        <button key={breed}>{breed}</button>
+        <button key={breed} onClick = {() => setBreed(breed)}>{breed}</button>
       )}
       <div className='dogs-list'>
         {dogs.length !== 0 &&
